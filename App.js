2c91fda7d6821c3389e94b76746d1db03eb87c80
Leo Carey-Williams <leocareywilliams@gmail.com>
2020-10-07 18:06:07 +0100

    navigation glitch fixed and set up complete

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import React from 'react'
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { BlogProvider } from './src/context/BlogContext';
 import IndexScreen from './src/screens/IndexScreen';
 
 const navigator = createStackNavigator({
   Index: IndexScreen
-}, {
-  initialRouteName: 'Index',
-  defaultNavigationOptions: {
-    title: 'Blog'
+},
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blog'
+    }
   }
-});
+);
+
+const App = createAppContainer(navigator)
+
+export default () => {
+  return <BlogProvider>
+    < App />
+  </BlogProvider>
+};
